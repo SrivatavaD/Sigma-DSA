31509c971cbf848a143a10e9743d455b814c043d
SrivastavaD <sdevansh296@gmail.com>
2024-08-10 12:56:00 +0530

    Updated.

--- Recursion/recursionFive.java
+++ Recursion/recursionFive.java
@@ -1,13 +1,34 @@
 public class recursionFive{
-    public static int power(int x, int n){
+
+    // Q:- Print x^n.
+    // public static int power(int x, int n){
+    //     if(n==0){
+    //         return 1;
+    //     }
+    //     return x * power(x,n-1);
+    // }
+    // public static void main(String [] args){
+    //     System.out.println(power(2, 10));
+
+    // }
+
+    // Optimized power function code
+
+    public static int optimizedPower(int a , int n){
         if(n==0){
             return 1;
         }
-        return x * power(x,n-1);
+        int halfPowerSq = optimizedPower(a, n/2) * optimizedPower(a, n/2);
+        //n is odd
+        if(n % 2 !=0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+        return halfPowerSq;
     }
-    public static void main(String [] args){
-        System.out.println(power(2, 10));
-
+    public static void main(String[] args) {
+        int a = 2;
+        int n = 10;
+        System.out.println(optimizedPower(a, n));
     }
     
 }
