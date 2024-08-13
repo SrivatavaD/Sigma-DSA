d81add3f24e47fade7d12ab43fdff267e6e7e06d
SrivastavaD <sdevansh296@gmail.com>
2024-08-13 10:31:11 +0530

    new question.

--- Recursion/recursionEight.java
+++ Recursion/recursionEight.java
@@ -3,21 +3,35 @@ public class recursionEight {
     // "two zero one nine". Use a recursive function to solve this problem .
     // The digits of the number will be only be in integer range 0-9 and last digit
     // of a number can't be 0.
-    static String digits[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    // static String digits[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-    public static void printDigits(int number) {
-        if (number == 0) {
-            return;
+    // public static void printDigits(int number) {
+    //     if (number == 0) {
+    //         return;
 
+    //     }
+
+    //     int lastDigits = number % 10;
+    //     printDigits(number / 10);
+    //     System.out.print(digits[lastDigits] + " ");
+    // }
+
+    // public static void main(String[] args) {
+    //     printDigits(1234);
+    //     System.out.println();
+    // }
+
+    // Write a program to find length of srting using recursion.
+    public static int length(String str){
+        if(str.length() == 0){
+            return 0;
         }
+        return length(str.substring(1)) + 1;
 
-        int lastDigits = number % 10;
-        printDigits(number / 10);
-        System.out.println(digits[lastDigits] + " ");
     }
-
     public static void main(String[] args) {
-        printDigits(1234);
-        System.out.println();
+        String str = "abcde";
+        System.out.println(length(str));
+
     }
 }
