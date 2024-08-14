a1e956393c42522590d1e250c44061be3852b3f5
SrivastavaD <sdevansh296@gmail.com>
2024-08-14 14:48:42 +0530

    Tower of hanoi.

--- Recursion/recursionNine.java
+++ Recursion/recursionNine.java
@@ -1,5 +1,20 @@
 public class recursionNine {
     // Tower of hanoi (important question)
-    
+    public static void towerOfHanoi(int n, String src, String helper, String dest){
+        if(n==1){
+            System.out.println("transfer disk " + n + " from " + src + " to" + dest);
+            return;
+        }
+        //transfer top n-1 from src to helper using dest as helper
+        towerOfHanoi(n-1, src, helper, dest);
+        //transfer nth from src to dest
+        System.out.println("transfer disk " + n + " from " + src + " to " + helper);
+        //transfer n-1 from helper to dest using src as "helper"
+        towerOfHanoi(n-1,src,  helper, dest);
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        towerOfHanoi(n, "A", "B", "C");
+    }
     
 }
