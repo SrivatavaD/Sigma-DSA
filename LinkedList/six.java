32f1ce5540459327d4d9bca18d3bf43476f1906a
SrivastavaD <sdevansh296@gmail.com>
2024-10-15 22:07:29 +0530

    Adding new question.

---
+++ LinkedList/six.java
@@ -0,0 +1,98 @@
+package LinkedList;
+
+public class six {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+
+    // Method to add node at the end of the list
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Method to print the linked list
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public void zigZag() {
+        // Find the middle of the list
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node mid = slow;
+
+        // Reverse the second half of the list
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Alternate merging - zig-zag merge
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public static void main(String[] args) {
+        six ll = new six();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+
+        // Print list before zig-zag
+        System.out.println("Before Zig-Zag:");
+        ll.print();
+
+        ll.zigZag();
+
+        // Print list after zig-zag
+        System.out.println("After Zig-Zag:");
+        ll.print();
+    }
+}
